ba9ba60d1fc4c8a30da7c7fb7652648b5825d9ee
oussama belkhadir <osamaalpha1997@gmail.com>
2022-09-14 19:43:26 +0100

    preparing array to be sorted and show some shuffled numbers

--- main.js
+++ main.js
@@ -1,27 +1,88 @@
-// Manual Generation
+/**************************************************************************** */
+/****************************************************** [ global ] ********** */
+let range;
+let length;
 
-let start = document.getElementById("start");
-let end = document.getElementById("end");
+/**************************************************************************** */
+/****************************************************** [ shuffle ] ********** */
+function shuffle(range) {
+	let result = [];
+	let n = range.length;
+	let i;
+	while (n) {
+		i = Math.floor(Math.random() * range.length);
+		if (i in range) {
+			result.push(range[i]);
+			delete range[i];
+			n--;
+		}
+	}
+	return result;
+}
+/**************************************************************************** */
+
+/**************************************************************************** */
+/**************************************************** [ Generator ] ********** */
+function generator(start, end)
+{
+	length = end - start;
+	range = Array(length);
+	for (let i = 0; i <= length; i++)
+		range[i] = start++;
+	console.log(range);
+	let shuffled = shuffle(range);
+	console.log(shuffled);
+}
+/**************************************************************************** */
+/******************************************* [ Manual Generation ] ********** */
+let startInput = document.getElementById("start");
+let endInput = document.getElementById("end");
 let manualBtn = document.querySelector(".btn_gen_manual")
-
 manualBtn.addEventListener("click", m_gen);
 function m_gen()
 {
-	if(start.value && end.value)
+	if(startInput.value && endInput.value)
 	{
-		if (start.value < end.value)
+		let start = Number(startInput.value);
+		let end = Number(endInput.value);
+		if (start < end)
 		{
-			console.log(start.value);
-			console.log(end.value);
+			generator(start, end);
+			startInput.value = "";
+			endInput.value = "";
 		}
-		else if (start.value == end.value)
-		{
-			alert("( -_- ), crazy man");
-		}
+		else if (start == end)
+			alert("( -_- ), You Are Crazy!!");
 		else
 			alert("\"start\" < \"end\", Please use your brain !!");
 	}
 	else
 		alert("\"start\" or \"end\" Missing");
-	// console.log("osama");
 }
+/**************************************************************************** */
+
+/**************************************************************************** */
+/********************************************* [ Auto Generation ] ********** */
+let howMany = document.getElementById("howMany");
+let autoBtn = document.querySelector(".btn_gen_auto");
+autoBtn.addEventListener("click", a_gen);
+function a_gen()
+{
+	if(Number(howMany.value) > 0)
+	{
+		let start = Math.floor(-Number(howMany.value)/2);
+		let end = Math.floor(+Number(howMany.value)/2);
+		console.log(start);
+		console.log(end);
+		generator(start, end)
+		howMany.value = "";
+	}
+	else
+		alert("Sorry, But You Are Not Reasonable At All!");
+}
+/**************************************************************************** */
+
+/**************************************************************************** */
+/********************************************* [ Display Numbers ] ********** */
+
+/**************************************************************************** */
